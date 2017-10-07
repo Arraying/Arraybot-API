97c9b9d624686fb4e0cf86e59e5ae4bd890081b2
Arraying <acvcnet@gmail.com>
2017-10-07 21:27:40 +0200

    Initial commit (contains pretty much nothing).

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+// Imports
+import (
+	"log"
+)
+
+// The main function.
+func main() {
+	log.Println("Starting up the Arraybot API...")
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Arraybot API
+
+This is a WIP repository for the Arraybot API that will be handling languages primarily, likely other things too.
